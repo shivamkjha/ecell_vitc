c272e5faa1ed07db8f07778e9c5fd16b98ff63c7
Shivam Kumar Jha <shivamjhaaaa@gmail.com>
2023-12-23 20:23:25 +0530

    debugging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { Outlet } from "react-router";
-
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <div className="flex flex-col min-h-screen bg-gray-100">
         <Navbar />
         <div className="flex-1 pt-16 pb-4">
-        <Outlet />
+          <Outlet />
         </div>
         <Footer />
       </div>
